c5ef1512207516b300265df54557ceb6f3971c9b
Jose-Henrique-a <henriquecruise@hotmail.com>
2024-04-13 23:36:39 -0300

    fix

---
+++ src/contas/ContaComum.java
@@ -0,0 +1,31 @@
+package contas;
+
+import classes.Pessoa;
+import classes.Taxas;
+
+public final class ContaComum extends Conta implements Taxas{
+
+
+    public ContaComum(){
+    }
+    public ContaComum(int numero, Pessoa correntista, float valor){
+        super(numero, correntista, valor);
+    }
+    public boolean sacar(float valor){
+        if(this.getSaldo() >= valor){
+            this.setSaldo(this.getSaldo() - valor);
+            return true;
+        }
+        return false;
+    }
+
+    public float getTaxaManutencao(){
+        return 15.00f;
+    }
+    public void descontarTaxaManutencao(){
+        this.setSaldo(this.getSaldo()- this.getTaxaManutencao());
+    }
+
+
+
+}
