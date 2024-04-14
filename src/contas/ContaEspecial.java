e1edd73bf8bbbb9510dd4ba2c05b3f49994c6d30
Jose-Henrique-a <henriquecruise@hotmail.com>
2024-04-14 15:05:08 -0300

    refactor

---
+++ src/contas/ContaEspecial.java
@@ -0,0 +1,48 @@
+package contas;
+
+import classes.Pessoa;
+import classes.Taxas;
+
+public final class ContaEspecial extends Conta implements Taxas{
+
+    private float limite;
+// Construtor 
+    public ContaEspecial(){
+
+    }
+    public ContaEspecial(int numero, Pessoa correntista, float valor, float limite){
+        super(numero, correntista, valor);
+        this.setLimite(limite);
+    }
+
+    public float getLimite() {
+        return limite;
+    }
+
+    public void setLimite(float limite){
+        this.limite = limite;
+    }
+
+    public boolean sacar(float valor){
+        if(this.getSaldo() - valor >= this.getLimite()){
+            this.setSaldo(this.getSaldo() - valor);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public float getTaxaManutencao(){
+        return 15.00f;
+    }
+    public void descontarTaxaManutencao(){
+        this.setSaldo(this.getSaldo()- this.getTaxaManutencao());
+    }
+
+
+
+
+
+
+
+}
