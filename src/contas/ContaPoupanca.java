0fd6ec0e7d324795a5a23c51390a7ed490ed9e90
Jose-Henrique-a <henriquecruise@hotmail.com>
2024-04-15 13:10:50 -0300

    test

---
+++ src/contas/ContaPoupanca.java
@@ -0,0 +1,25 @@
+package contas;
+
+import classes.Pessoa;
+
+public class ContaPoupanca extends Conta{
+   
+    public ContaPoupanca(){
+
+    }
+    public ContaPoupanca(int numero, Pessoa correntista, float valor){
+        super(numero, correntista, valor);
+    }
+    
+    public boolean sacar(float valor){
+        if(this.getSaldo() >= valor){
+            this.setSaldo(this.getSaldo() - valor);
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+}
